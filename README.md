40ff783d23a9282cecd23573b87e12b7b984b02c
Akos Kitta <kittaakos@typefox.io>
2018-10-02 15:10:49 +0200

    Added a customized p2 site.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

---
+++ README.md
@@ -0,0 +1,7 @@
+# lsp4j-type-hierarchy
+A p2 update site of the LSP4J with semantic highlighting capabilities
+
+This is a hack to produce a p2 update site for LSP4J with the type hierarchy capabilities.
+To update the content. Run `./update.sh` from the repository root.
+
+p2 does some black magic caching, so if your TP does not pick up the new version, go to `{workspace}/.metadata/.plugins/org.eclipse.pde.core/` and wipe the content, restart Eclipse and reload the TP.
---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script to build and update this p2 update site with the most recent LSP4J state with the type hierarchy support.
+
+rm -rf ./lsp4j/ \
+&& git clone --single-branch -b type-hierarchy https://github.com/kittaakos/lsp4j.git \
+&& ./lsp4j/gradlew -p lsp4j build install generateP2Build --refresh-dependencies \
+&& mvn -f ./lsp4j/releng/pom.xml clean verify \
+&& rm -rf ./p2/ \
+&& unzip -o ./lsp4j/releng/p2/target/lsp4j.p2-repository-0.6.0-SNAPSHOT.zip -d ./p2/
