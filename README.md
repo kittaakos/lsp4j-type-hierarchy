aa7d0f53668c7548fe34b7e9c73aeb1a739e3ca2
Akos Kitta <kittaakos@typefox.io>
2018-10-09 09:34:48 +0200

    Updated to: 08d3bc1e1c7e5b5fe06252f3d20599a1bc01620b
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # lsp4j-type-hierarchy
-A p2 update site of the LSP4J with the type hierarchy capabilities
+A p2 update site of the LSP4J with the type hierarchy capabilities.
+See [here](https://github.com/eclipse/lsp4j/issues/272) for more details.
 
 This is a hack to produce a p2 update site for LSP4J with the type hierarchy capabilities.
 To update the content. Run `./update.sh` from the repository root.
--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 # Script to build and update this p2 update site with the most recent LSP4J state with the type hierarchy support.
 
 rm -rf ./lsp4j/ \
-&& git clone --single-branch -b type-hierarchy https://github.com/kittaakos/lsp4j.git \
+&& git clone --single-branch -b GH-272 https://github.com/kittaakos/lsp4j.git \
 && ./lsp4j/gradlew -p lsp4j build install generateP2Build --refresh-dependencies \
 && mvn -f ./lsp4j/releng/pom.xml clean verify \
 && rm -rf ./p2/ \
